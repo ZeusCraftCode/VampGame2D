0d95e0b64af1f0f3da5bca6c5719017e9363b12b
UnderLeonGC <106019888+UnderLeonGC@users.noreply.github.com>
2025-01-03 18:20:45 +0100

    Inventar ver.1.0

--- scripts/spieler.gd
+++ scripts/spieler.gd
@@ -6,6 +6,8 @@ const JUMP_VELOCITY = -300.0
 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
+@export var inv: Inv
+
 #Laufen &S pringen
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
